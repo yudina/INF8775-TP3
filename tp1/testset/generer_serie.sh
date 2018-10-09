dca57c734e1d8285932fcbfa2c0c7e1ef6060baa
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-10-09 13:34:30 -0400

    Ajout des exemplaires dans dossier testset
    Pour faciliter la réutilisation des scripts

---
+++ tp1/testset/generer_serie.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ce script génère une série d'exemplaires
+# Argument 1 : N de départ
+# Argument 2 : nombre de N
+# Argument 3 : germe
+
+Nstart=$1
+Nend=$((Nstart+$2-1))
+germe=$3
+
+for((N=$Nstart;N<=$Nend;N++)); do
+    for I in {1..5}; do
+	py generer_exemplaire.py $N "ex_"$N"."$I $((germe+$I))
+    done
+done
