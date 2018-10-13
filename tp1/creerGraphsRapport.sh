50114bdb89b08150e76d6cb857604e338d5dfaae
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-10-13 12:50:14 -0400

    Graphes faits

--- tp1/creerGraphsRapport.sh
+++ tp1/creerGraphsRapport.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
-for data in $(ls results/tests/rapport_opt/)
+for data in $(ls results/tests/rapport/)
 do
-	sed -i 's/,/ /g' results/tests/rapport_opt/$data
-	gnuplot -e "set xlabel \"Taille\" offset 2.1; 
-	set ylabel \"Temps\" offset 2.1;
+	sed -i 's/,/ /g' results/tests/rapport/$data
+	gnuplot -e "set xlabel \"Taille de l'exemplaire 2^N\" offset 2.1; 
+	set ylabel \"Rapport du temps sur hypothese f(x)\" offset 2.1;
 	set autoscale xy; 
 	set term png;
 	set term png size 800, 650; 
-	set output \"graphs/${data%.csv}_rapport_opt.png\"; 
-	plot \"results/tests/rapport_opt/$data\" with linespoints"
-done
-
-for data in $(ls results/tests/rapport_worst/)
-do
-	sed -i 's/,/ /g' results/tests/rapport_worst/$data
-	gnuplot -e "set xlabel \"Taille\" offset 2.1; 
-	set ylabel \"Temps\" offset 2.1;
-	set autoscale xy; 
-	set term png;
-	set term png size 800, 650; 
-	set output \"graphs/${data%.csv}_rapport_worst.png\"; 
-	plot \"results/tests/rapport_worst/$data\" with linespoints"
+	set output \"graphs/${data%.csv}_rapport.png\"; 
+	plot \"results/tests/rapport/$data\" with linespoints"
 done
