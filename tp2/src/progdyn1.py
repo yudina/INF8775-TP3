f616d8c5501aad2a93edd4a2ae0e1bf0fdab68a1
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-11-02 13:31:06 -0400

    Création de fichiers src sur lesquels nous allons travailler

---
+++ tp2/src/progdyn1.py
@@ -0,0 +1,11 @@
+import sys
+
+ex_path = sys.argv[1] # Path de l'exemplaire
+
+# TODO: Algo ici
+
+options = sys.argv[2:]
+if '-p' in options: # On imprime la solution
+    print("84 73 12 44 98 75") # Données bidon, mais output du bon format demandé
+if '-t' in options: # On imprime le temps d'exécution
+    print("4.1347628746") # Données bidon, mais output du bon format demandé
