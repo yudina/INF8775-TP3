bfc54b55b2a095385e2a883a5bb2d6bdd9b94536
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-11-02 13:38:57 -0400

    tableaux marginaux pour contenir les bâtons

--- tp2/src/glouton.py
+++ tp2/src/glouton.py
@@ -2,13 +2,20 @@ import sys
 
 ex_path = sys.argv[1] # Path de l'exemplaire
 
-# TODO: Algo ici
+def glouton(poids, poidsMax):
+    
+    # index = [0, 1, 2, ..., n - 1] for n items
+    index = list(range(len(value)))
+    # contains ratios of values to weight
+    ratio = [v/w for v, w in zip(value, weight)]
+    # index is sorted according to value-to-weight ratio in decreasing order
+    index.sort(key=lambda i: ratio[i], reverse=True)
+    
+    
+    
 
 options = sys.argv[2:]
 if '-p' in options: # On imprime la solution
     print("84 73 12 44 98 75") # Données bidon, mais output du bon format demandé
 if '-t' in options: # On imprime le temps d'exécution
     print("4.1347628746") # Données bidon, mais output du bon format demandé
-    
-
-def glouton(poids, poidsMax):
