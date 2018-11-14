43194e209a39699bb33220b016fa2a3e693f58e5
Yujia Ding <yujia.ding@polymtl.ca>
2018-11-14 17:27:56 -0500

    script to run everything, result excel file, started report

--- tp2/exemple/src/glouton.py
+++ tp2/exemple/src/glouton.py
@@ -4,7 +4,7 @@ import time
 ex_path = sys.argv[1] # Path de l'exemplaire
 
 # TODO: Algo ici
-
+start = time.process_time()
 poids = []
 poidsMax = 0
 
@@ -15,27 +15,18 @@ text_file.close()
 
 poids = lines[1::2]
 poidsMax = lines[-1]
-poids = sorted(poids, key=int, reverse=True) # Trie les bâtons en ordre décroissant
+poids = sorted(poids, key=int, reverse=True) 
 
 nbDyn = len(poids)
 
-start = time.process_time()
-sommeBatons = 0
-solution = []
+sum = 0
 for i in range (nbDyn):
-    if sommeBatons + int(poids[i]) < int(poidsMax):
-        sommeBatons += int(poids[i])
-        solution.append(int(poids[i]))
+    if sum + int(poids[i]) < int(poidsMax):
+        sum += int(poids[i])
 end = time.process_time()
-
-def imprimerSolution(s):
-    sortie = ""
-    for i in range(len(s)):
-        sortie += str(s[i]) + " "
-    print(sortie)
 
 options = sys.argv[2:]
 if '-p' in options: # On imprime la solution
-    imprimerSolution(solution)
+    print(sum)
 if '-t' in options: # On imprime le temps d'exécution
-    print(end - start)
+    print(end - start, "seconds")
---
+++ tp2/exemple/src/glouton2.py
@@ -0,0 +1,41 @@
+import sys
+import time
+
+ex_path = sys.argv[1] # Path de l'exemplaire
+
+# TODO: Algo ici
+
+poids = []
+poidsMax = 0
+
+text_file = open(ex_path, "r")
+next(text_file)
+lines = text_file.read().split()
+text_file.close()
+
+poids = lines[1::2]
+poidsMax = lines[-1]
+poids = sorted(poids, key=int, reverse=True) # Trie les bâtons en ordre décroissant
+
+nbDyn = len(poids)
+
+start = time.process_time()
+sommeBatons = 0
+solution = []
+for i in range (nbDyn):
+    if sommeBatons + int(poids[i]) < int(poidsMax):
+        sommeBatons += int(poids[i])
+        solution.append(int(poids[i]))
+end = time.process_time()
+
+def imprimerSolution(s):
+    sortie = ""
+    for i in range(len(s)):
+        sortie += str(s[i]) + " "
+    print(sortie)
+
+options = sys.argv[2:]
+if '-p' in options: # On imprime la solution
+    imprimerSolution(solution)
+if '-t' in options: # On imprime le temps d'exécution
+    print(end - start)
