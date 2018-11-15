ba1f0764f39eb88e112321faaff2c5378ef29608
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-11-14 22:29:40 -0500

    renommage et restructuration recuit

--- tp2/exemple/src/recuit.py
+++ tp2/exemple/src/recuit.py
@@ -9,12 +9,12 @@ ex_path = "PR_100_400_1.txt"#sys.argv[1] # Path de l'exemplaire
 # variables globales (pour remplacer les passages par référence)
 poids = []
 poidsMax = 0
-nBatons = 0
+nbDyn = 0
 debut = 0
 fin = 0
 temps = 0
 
-def extraireListeBaton():
+def extraireListeBatons():
     text_file = open(ex_path, "r")
     next(text_file)
     lines = text_file.read().split()
@@ -22,21 +22,20 @@ def extraireListeBaton():
     
     global poids
     global poidsMax
-    global nBatons
+    global nbDyn
     poids = lines[1::2]
-    poidsMax = lines[-1]
+    poidsMax = int(lines[-1])
     poids = sorted(poids, key=int, reverse=True) # Trie les bâtons en ordre décroissant
-    
-    nBatons = len(poids)
+    poids = [ int(x) for x in poids ]
+    nbDyn = len(poids)
 
 def glouton():
-    global nBatons
     sommeBatons = 0
     solution = []
-    for i in range (nBatons):
-        if sommeBatons + int(poids[i]) < int(poidsMax):
-            sommeBatons += int(poids[i])
-            solution.append(int(poids[i]))
+    for i in range (nbDyn):
+        if sommeBatons + poids[i] < poidsMax:
+            sommeBatons += poids[i]
+            solution.append(poids[i])
     return solution
  
 # La fonction retourne le poids total d’une liste de baton
@@ -49,72 +48,76 @@ def somme(batons):
     return sommePoids
 
 # https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
-def choisirAleatoireBaton(batonsRestants):
-    indexAleatoire = randrange(len(batonsRestants))
+def choisirAleatoireBaton(batons):
+    indexAleatoire = randrange(len(batons))
     return indexAleatoire;
 
 #https://stackoverflow.com/questions/627435/how-do-i-remove-an-element-from-a-list-by-index-in-python
-def voisin(poidsMax, batons, resultat):
+def voisin(poidsMax, poidsPrime, SPrime):
     #    Une solution voisine est obtenue en choisissant uniformément
     #    au hasard un bâton parmi ceux qui ne sont pas encore choisis
     #    et en l’ajoutant à la solution
-    nouveauBaton = choisirAleatoireBaton(batons)
-    resultat.append(batons[nouveauBaton])
-    del batons[nouveauBaton]
+    nouveauBaton = choisirAleatoireBaton(poidsPrime)
+    SPrime.append(poidsPrime[nouveauBaton])
+    del poidsPrime[nouveauBaton]
     
     #    Il est possible que cela rende le poids total supérieur à P :
     #    on retirera alors des bâtons un à un (choisis uniformément au
     #    hasard) jusqu’à ce que le poids total ne dépasse pas P
-    while int(poidsMax) < somme(resultat):
-        resultatRetire = choisirAleatoireBaton(resultat)
-        batons.append(resultat[resultatRetire])
-        del resultat[resultatRetire]
+    while int(poidsMax) < somme(SPrime):
+        resultatRetire = choisirAleatoireBaton(SPrime)
+        poidsPrime.append(SPrime[resultatRetire])
+        del SPrime[resultatRetire]
 
-#    HOW YOU DO THIS ? : c'est déjà fait par globales i gauss
-#    return #solution voisine de la solution en paramètre
-
-def recuit(poidsMax, batons, resultat):#(S0, T, kmax, P, α):
-    
-    # resultat c'est S0
+def recuit(poidsMax, poids, S0):#(S0, T, kmax, P, α):
     theta = 100 # T
     kmax = 5
     P = 5
     alpha = 0.8
-
-    solutionOptimale = resultat
+    
+    solutionRecuit = S0
     
     for i in range (kmax):
         for j in range (P):
-            solutionAlternative = resultat
-            batonsAlternatives = batons
-            voisin(poidsMax, batonsAlternatives, solutionAlternative)
-            delta = somme(solutionAlternative) - somme(resultat)
+            SPrime = S0
+            poidsPrime = poids
+            
+            print()
+            voisin(poidsMax, poidsPrime, SPrime)
+            
+            delta = somme(SPrime) - somme(S0)
             exposant = math.exp(delta/theta)
             if (0 <= delta or random.uniform(0, 1) <= exposant ):
-                batons = batonsAlternatives
-                resultat = solutionAlternative
-                if (somme(solutionOptimale) <= somme(solutionAlternative)):
-                    solutionOptimale = resultat
+                poids = poidsPrime
+                S0 = SPrime
+                if (somme(solutionRecuit) <= somme(SPrime)):
+                    solutionRecuit = S0
         theta = theta * alpha
-        return solutionOptimale
+        return solutionRecuit
 
 def imprimerSolution(s):
     sortie = ""
+    sommeBatons = 0
     for i in range(len(s)):
+        sommeBatons += s[i]
         sortie += str(s[i]) + " "
     print(sortie)
+    print(sommeBatons)
 
-extraireListeBaton()
+# Le "main"
+extraireListeBatons()
+
 debut = time.process_time()
-resultat = glouton()
-solutionOptimale = recuit(poidsMax, poids, resultat)
+S0 = glouton()
+solutionRecuit = recuit(poidsMax, poids, S0)
 fin = time.process_time()
 temps = fin - debut
-imprimerSolution(solutionOptimale)
+
+imprimerSolution(solutionRecuit)
 print(temps)
 
 options = sys.argv[2:]
 if '-p' in options: # On imprime la solution
-    imprimerSolution(solutionOptimale) # Données bidon, mais output du bon format demandé
+    imprimerSolution(solutionRecuit)
 if '-t' in options: # On imprime le temps d'exécution
-    print(temps) # Données bidon, mais output du bon format demandé
+    print(temps)
