aec58e0d7236ec342ac6006abf688a46c8bc4f46
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-11-14 21:14:25 -0500

    commentaires dans glouton, revert suppression

--- tp2/exemple/src/glouton2.py
+++ tp2/exemple/src/glouton2.py
@@ -3,35 +3,36 @@ import time
 
 ex_path = sys.argv[1] # Path de l'exemplaire
 
-# TODO: Algo ici
-
 poids = []
 poidsMax = 0
-
+    
+# lire exemplaire
 text_file = open(ex_path, "r")
 next(text_file)
 lines = text_file.read().split()
 text_file.close()
 
+# extraire batons
 poids = lines[1::2]
-poidsMax = lines[-1]
+poidsMax = int(lines[-1])
 poids = sorted(poids, key=int, reverse=True) # Trie les bâtons en ordre décroissant
-
+poids = [ int(x) for x in poids ]
 nbDyn = len(poids)
 
+# algo de glouton
 start = time.process_time()
 sommeBatons = 0
 solution = []
 for i in range (nbDyn):
-    if sommeBatons + int(poids[i]) < int(poidsMax):
-        sommeBatons += int(poids[i])
-        solution.append(int(poids[i]))
+    if sommeBatons + poids[i] < poidsMax:
+        sommeBatons += poids[i]
+        solution.append(poids[i])
 end = time.process_time()
 
-def imprimerSolution(s):
+def imprimerSolution(solution):
     sortie = ""
-    for i in range(len(s)):
-        sortie += str(s[i]) + " "
+    for i in range(len(solution)):
+        sortie += str(solution[i]) + " "
     print(sortie)
 
 options = sys.argv[2:]
--- tp2/src/glouton.py
+++ tp2/src/glouton.py
@@ -5,20 +5,21 @@ ex_path = "PR_100_400_1.txt"#sys.argv[1] # Path de l'exemplaire
 
 poids = []
 poidsMax = 0
-
+    
+# lire exemplaire
 text_file = open(ex_path, "r")
 next(text_file)
 lines = text_file.read().split()
 text_file.close()
 
+# extraire batons
 poids = lines[1::2]
 poidsMax = int(lines[-1])
 poids = sorted(poids, key=int, reverse=True) # Trie les bâtons en ordre décroissant
-
 poids = [ int(x) for x in poids ]
-
 nbDyn = len(poids)
 
+# algo de glouton
 start = time.process_time()
 sommeBatons = 0
 solution = []
@@ -26,9 +27,6 @@ for i in range (nbDyn):
     if sommeBatons + poids[i] < poidsMax:
         sommeBatons += poids[i]
         solution.append(poids[i])
-        del poids[i]
-    else:
-        break
 end = time.process_time()
 
 def imprimerSolution(solution):
@@ -36,8 +34,6 @@ def imprimerSolution(solution):
     for i in range(len(solution)):
         sortie += str(solution[i]) + " "
     print(sortie)
-
-imprimerSolution(solution)
 
 options = sys.argv[2:]
 if '-p' in options: # On imprime la solution
