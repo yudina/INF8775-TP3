bc47b6eab9e983db7a73ea22beebbb5bb59bfb8f
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-11-16 10:22:29 -0500

    rapport remise

---
+++ tp2/creerGraphsRapport.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for data in $(ls results/tests/rapport/)
+do
+	sed -i 's/,/ /g' results/tests/rapport/$data
+	gnuplot -e "set xlabel \"Taille de l'exemplaire (n)\" offset 2.1; 
+	set ylabel \"Rapport du temps sur hypothese f(x)\" offset 2.1;
+	set autoscale xy; 
+	set term png;
+	set term png size 800, 650; 
+	set output \"graphs/${data%.csv}_rapport.png\"; 
+	plot \"results/tests/rapport/$data\" with linespoints"
+done
