e5bad45bf488bfda66937d72a078189dc108e79c
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-12-01 18:57:15 -0500

    commentaires pour extractData

--- tp3/visite.py
+++ tp3/visite.py
@@ -4,7 +4,6 @@ import time
 from math import ceil, log
 
 def extractData(file_name):
-    
     adj_matrix = []
     
     # obtain every line as int list
@@ -14,18 +13,19 @@ def extractData(file_name):
             if len(line) > 1:
                adj_matrix.append([int(a) for a in line.split()])
            
+    # collect data
     n_sites = adj_matrix[0][0]
     max_time = adj_matrix[-2][0]
     popularity = adj_matrix[-1]
     
-    #delete first, second to last and last rows
+    # delete first, second to last and last rows
     adj_matrix.remove(adj_matrix[0])
     adj_matrix.remove(adj_matrix[-2])
     adj_matrix.remove(adj_matrix[-1])
     
-    print(n_sites)
-    print(max_time)
-    print(popularity)
-    print(adj_matrix)
+#    print(n_sites)
+#    print(max_time)
+#    print(popularity)
+#    print(adj_matrix)
 
 extractData("./instances/PCT_20_50");
