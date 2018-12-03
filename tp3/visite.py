e1f21e1f4d08f1d22fb6be93a9b1701ec0097c5f
Kevin Chan <ka-hin-kevin.chan@polymtl.ca>
2018-12-02 21:08:57 -0500

    refactor beginTravel

--- tp3/visite.py
+++ tp3/visite.py
@@ -7,11 +7,13 @@ from math import ceil, log
 
 ex_path = "./instances/PCT_20_50" #sys.argv[1]
 
+cumul_time = 0
 relative_e = 0.20
 total_popularity = 0
 n_sites = 0
 adj_matrix = [] # row will be start point, while column will be endpoint
 density_adj = []
+avail_popularity = []
 max_time = 0
 popularity = []
 travel = [] # index, popularity, time
@@ -55,42 +57,51 @@ def calculateDensityAdj():
             else:
                 density_adj[i][j] = adj_matrix[i][j]
 
+def randomBeginTravel():
+    global travel
+    global cumul_time
+    global avail_popularity
+    
+    # generate n_sites*0.6 random indexes for the beginning of the travel
+    n_random_sites = int( (n_sites*0.6) )
+    i_random_sites = random.sample(range(1, n_sites), n_random_sites)
+    
+    cumul_time = 0
+    
+    # choose random sites
+    for i in range(1, n_random_sites):
+        if (cumul_time <= max_time):
+            i_previous_site = travel[i-1][0]
+            new_time = adj_matrix[i_previous_site][i_random_sites[i]]
+            
+            # add the next random site.
+            # index, popularity, time (row = previous site, column = current site)
+            travel.append((i_random_sites[i], popularity[i_random_sites[i]], adj_matrix[i_previous_site][i_random_sites[i]]))
+            cumul_time += new_time
+            if (cumul_time >= max_time):
+                cumul_time -= new_time
+                break
+        else:
+            break
+    
+    # remove visited, and place non-visited in a copy
+    visited = sorted(i_random_sites, key=int, reverse=True)
+    avail_popularity = popularity.copy()
+    
+    for i in range(len(visited)):
+        j = visited[i]
+        avail_popularity.remove(avail_popularity[j])
+    
+    
 # THE MAIN
 extractData(ex_path);
 start_time = time.time()
 calculateDensityAdj();
 travel.append((I_HOTEL, POP_HOTEL, adj_matrix[0][0])) # go to hotel
+randomBeginTravel();
+
+
 # END OF THE MAIN
-
-# generate some random indexes for the beginning of the travel
-n_random_sites = int( (n_sites*0.6) )
-i_random_sites = random.sample(range(1, n_sites), n_random_sites)
-
-cumul_time = 0
-
-for i in range(1, n_random_sites):
-    if (cumul_time <= max_time):
-        i_previous_site = travel[i-1][0]
-        new_time = adj_matrix[i_previous_site][i_random_sites[i]]
-        # add the next random site.
-        # index, popularity, time (row = previous site, column = current site)
-        travel.append((i_random_sites[i], popularity[i_random_sites[i]], adj_matrix[i_previous_site][i_random_sites[i]]))
-        cumul_time += new_time
-        if (cumul_time >= max_time):
-            cumul_time -= new_time
-            break
-    else:
-        break
-    
-sites_visited = sorted(i_random_sites, key=int, reverse=True)
-popularity_avail = popularity.copy()
-
-#print("adj", adj_matrix, "pop", popularity)
-
-for i in range(len(sites_visited)):
-    j = sites_visited[i]
-    popularity_avail.remove(popularity_avail[j])
-
 
 print(density_adj)
 
